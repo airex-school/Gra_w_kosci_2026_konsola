12c7a38f8b291ac7a0b893c776acb9ae8e0e6bdf
Adam Kędryna <adam.kedryna@gmail.com>
2026-09-09 09:40:21 +0200

    Add project files.

---
+++ Kosc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_w_kosci_2026_konsola
+{
+    internal class Kosc
+    {
+        public static int LiczbaInstancji;
+        public string[] NazwyPlikow = {"kosc0.png", "kosc1.png", "kosc2.png", "kosc3.png", "kosc4.png", "kosc5.png", "kosc6.png"};
+        public int LiczbaOczek;
+        public int IdentyfikatorPlikuGraficznego;
+        public bool CzyDostepna;
+
+        public Kosc(int WartoscWyrzuconejKosci)
+        {
+            int[] DozwoloneLiczby = {1, 2, 3, 4, 5, 6};
+            if (!DozwoloneLiczby.Contains(WartoscWyrzuconejKosci)) {
+                WartoscWyrzuconejKosci = 0;
+            }
+
+            this.LiczbaOczek = WartoscWyrzuconejKosci;
+            this.IdentyfikatorPlikuGraficznego = WartoscWyrzuconejKosci;
+            this.CzyDostepna = true;
+            Kosc.LiczbaInstancji++;
+        }
+
+        public Kosc()
+        {
+            int LosowaLiczba = new Random().Next(1, 7);
+            this.LiczbaOczek = LosowaLiczba;
+            this.IdentyfikatorPlikuGraficznego = LosowaLiczba;
+            this.CzyDostepna = true;
+            Kosc.LiczbaInstancji++;
+        }
+
+        public void RzutKoscia()
+        {
+            if(CzyDostepna == true)
+            {
+                int LosowaLiczba = new Random().Next(1, 7);
+                this.LiczbaOczek = LosowaLiczba;
+                this.IdentyfikatorPlikuGraficznego = LosowaLiczba;
+            }
+        }
+
+        public void ZablokujKosc()
+        {
+            this.CzyDostepna = false;
+        }
+
+        public string ZwrocWyrzuconaWartosc()
+        {
+            string[] Wartosci = { "jeden", "dwa", "trzy", "cztery", "pięć", "sześć" };
+            string WartoscSlownie = Wartosci[this.LiczbaOczek - 1];
+            return WartoscSlownie;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Gra_w_kosci_2026_konsola;
+Console.WriteLine("Wprowadz liczbe od 1 do 6");
+int WprowadzLiczbe = Int32.Parse(Console.ReadLine());
+
+Kosc Kosc1 = new Kosc(WprowadzLiczbe);
+Console.WriteLine("liczba instancji: " + Kosc.LiczbaInstancji);
+Console.WriteLine("liczba oczek: " + Kosc1.LiczbaOczek);
+Console.WriteLine("nazwa pliku: " + Kosc1.NazwyPlikow[Kosc1.IdentyfikatorPlikuGraficznego]);
+
+Console.WriteLine();
+
+Kosc Kosc2 = new Kosc();
+Console.WriteLine("liczba instancji: " + Kosc.LiczbaInstancji);
+Console.WriteLine("liczba oczek: " + Kosc2.LiczbaOczek);
+Console.WriteLine("nazwa pliku: " + Kosc2.NazwyPlikow[Kosc2.IdentyfikatorPlikuGraficznego]);
